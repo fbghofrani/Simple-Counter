2bfb2e14752035d390766322196c124a17e4b318
fbghofrani <fbghofrani@yahoo.com>
2024-03-17 05:23:25 +0330

    edit 1

---
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.simplecounter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.simplecounter.ui.theme.SimplecounterTheme
+
+class MainActivity : ComponentActivity() {
+    var counter=0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SimplecounterTheme {
+                // A surface container using the 'background' color from the theme
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    )
+    {
+        var number by remember{
+            mutableStateOf(0);
+        }
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        )
+        {
+            TextResult("$number")
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround
+            )
+            {
+                MyButton("+", {number++})
+                MyButton("-", {number--})
+            }
+        }
+    }
+}
+
+@Composable
+private fun TextResult(s: String) {
+    Text(text = s, fontSize = 30.sp, fontWeight = Bold)
+}
+
+
+@Composable
+private fun MyButton(text: String, function: () -> Unit) {
+    Button(onClick = function) {
+        Text(text = text)
+    }
+}
+
+@Preview
+@Composable
+fun GreetingPreview() {
+    MyApp()
+}
